b781f06e7ac90c28f827aee91148060162fbcc2f
WinLeaf1239 <WinLeaf1239@tutamail.com>
2025-10-03 13:25:09 +0300

    1234

--- Prac if while/Untitled-1.py
+++ Prac if while/Untitled-1.py
@@ -20,8 +20,36 @@ while n != 0 :
     if Z > 1_000_000 :
         print("Бесконеч")
         break
-if Z < 1_000_000 :
+if Z <= 1_000_000 :
     print("Итоговое кол-во шагов " , Z)
+
+    """
+    a = (input()).split()
+b = 0
+for i in range(0 , len(a)) :
+    a[i] = int(a[i])
+x = int(input(''))
+match x :
+    case 0 :
+        for i in range(0 , len(a)) :
+            a[i] = 0
+    case 1 :
+        for i in range(0,len(a)):
+            a[i] = a[i] *2
+    case 2 :
+        for i in range(0, len(a) , 2) :
+            a[i] = a[i] +10
+    case 3 :
+        for i in range(0,len(a),3):
+            if a[i] >= 0:
+                a[i] = a[i] **0.5
+            else : 
+                a[i] = 0
+    case _:
+        b = sum(a)
+print(a)
+print(b)
+    """
 
 
 
---
+++ Prac if while/dfgdfgfgdgdd.py
@@ -0,0 +1,25 @@
+a = (input()).split()
+b = 0
+for i in range(0 , len(a)) :
+    a[i] = int(a[i])
+x = int(input(''))
+match x :
+    case 0 :
+        for i in range(0 , len(a)) :
+            a[i] = 0
+    case 1 :
+        for i in range(0,len(a)):
+            a[i] = a[i] *2
+    case 2 :
+        for i in range(0, len(a) , 2) :
+            a[i] = a[i] +10
+    case 3 :
+        for i in range(0,len(a),3):
+            if a[i] >= 0:
+                a[i] = a[i] **0.5
+            else : 
+                a[i] = 0
+    case _:
+        b = sum(a)
+print(a)
+print(b)
